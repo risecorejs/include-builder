e2dd38b43399a6ab0f46c5c326b56dfc4b79eef7
Abdulsalam Amenov <amenov.abdulsalam@gmail.com>
2022-09-06 10:05:16 +0600

    update to ts

--- middleware.ts
+++ middleware.ts
@@ -20,7 +20,7 @@ export default function (): express.Handler {
 
 declare global {
   namespace Express {
-    interface Request {
+    export interface Request {
       includeBuilder(includes: object, defaultIncludes: string[]): any[]
     }
   }
